ab1c2becbbba6186bf816744893c1e25eeb08bd7
GioGKS <gulikashvili205@gmail.com>
2023-06-10 20:26:05 +0300

    Started Validations

--- MoviesAPI/Entities/Genre.cs
+++ MoviesAPI/Entities/Genre.cs
@@ -1,10 +1,35 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using MoviesAPI.Validations;
+
 namespace MoviesAPI.Entities
 {
-	public class Genre
-	{
-		public int Id { get; set; }
-		public string Name { get; set; }
-	}
+    public class Genre : IValidatableObject
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "This Field with name {0} is required")]
+        [StringLength(10)]
+        //[FirstLetterUppercase]
+        public string Name { get; set; }
+
+        [Range(18, 120)]
+        public int age { get; set; }
+        [CreditCard]
+        public string CreditCard { get; set; }
+        [Url]
+        public string Url { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (!string.IsNullOrEmpty(Name))
+            {
+                var firstLetter = Name[0].ToString();
+                if (firstLetter != firstLetter.ToUpper())
+                {
+                    yield return new ValidationResult("First letter should be uppercase", new string[] { nameof(Name) });
+                }
+            }
+        }
+    }
 }
 
