a06065c355534c4819627ee0f7d5244fdfa1df59
GioGKS <gulikashvili205@gmail.com>
2023-06-11 23:19:59 +0300

    Filters

--- MoviesAPI/Controllers/GenresController.cs
+++ MoviesAPI/Controllers/GenresController.cs
@@ -1,16 +1,71 @@
 ﻿using System;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using MoviesAPI.Entities;
 using MoviesAPI.Services;
 
 namespace MoviesAPI.Controllers
 {
-	public class GenresController
+	[Route("api/genres")]
+    [ApiController]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    public class GenresController : ControllerBase
 	{
 		private readonly IRepository repository;
-
-		public GenresController(IRepository repository)
+        private readonly ILogger<GenresController> logger;
+		public GenresController(IRepository repository, ILogger<GenresController> logger)
 		{
 			this.repository = repository;
+            this.logger = logger;
 		}
-	}
+
+
+		[HttpGet] // api/genres
+        [HttpGet("list")] // api/genres/list
+        [HttpGet("/allgenres")] // allgenres
+        [ResponseCache(Duration = 60)]
+		public async Task<ActionResult<List<Genre>>> Get()
+		{
+            logger.LogInformation("Getting all the genres");
+			return await repository.GetAllGenres();
+		}
+
+        [HttpGet("{Id:int}")] // api/genres/example
+        public ActionResult<Genre> Get(int id, [BindNever] string param2)
+        {
+            logger.LogDebug("get by Id method executing...");
+            var genre = repository.GetGenreById(id);
+            if (genre == null)
+            {
+                logger.LogWarning($"Genre with Id {id} not found");
+                logger.LogError("this is an error");
+                return NotFound();
+            }
+
+            return genre;
+        }
+
+        [HttpPost]
+		public ActionResult Post([FromBody] Genre genre)
+		{
+            repository.AddGenre(genre);
+
+            return NoContent();
+		}
+
+        [HttpPost]
+        public ActionResult Put([FromBody] Genre genre)
+        {
+            return NoContent();
+        }
+
+        [HttpDelete]
+        public ActionResult Delete()
+        {
+            return NoContent();
+        }
+    }
 }
 
--- MoviesAPI/Startup.cs
+++ MoviesAPI/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -29,6 +30,8 @@ namespace MoviesAPI
         {
 
             services.AddControllers();
+            services.AddResponseCaching();
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer();
             services.AddSingleton<IRepository, InMemoryRepository>();
             services.AddSwaggerGen(c =>
             {
@@ -77,6 +80,10 @@ namespace MoviesAPI
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseResponseCaching();
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
