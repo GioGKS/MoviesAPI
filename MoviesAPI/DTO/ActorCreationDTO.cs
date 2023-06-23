3dad93cc0155605787e5bd9577d5fffa7f655938
GioGKS <gulikashvili205@gmail.com>
2023-06-23 20:22:06 +0300

    actor entity update

---
+++ MoviesAPI/DTO/ActorCreationDTO.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace MoviesAPI.DTO
+{
+	public class ActorCreationDTO
+	{
+		public ActorCreationDTO()
+		{
+		}
+	}
+}
+
---
+++ MoviesAPI/DTO/ActorDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace MoviesAPI.DTO
+{
+	public class ActorDTO
+	{
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime DateOfbirth { get; set; }
+        public string Biography { get; set; }
+        public string Picture { get; set; }
+    }
+}
+
