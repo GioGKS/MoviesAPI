16e7bf90d5a8f9cb9eceb8f7d7d8d6a03831d4c1
GioGKS <gulikashvili205@gmail.com>
2023-07-02 21:53:05 +0300

    Updated

--- MoviesAPI/Controllers/AccountsController.cs
+++ MoviesAPI/Controllers/AccountsController.cs
@@ -1,85 +1,134 @@
-﻿using System;
-using System.IdentityModel.Tokens.Jwt;
-using System.Security.Claims;
-using System.Text;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
 using MoviesAPI.DTO;
+using MoviesAPI.DTOs;
+using MoviesAPI.Helpers;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace MoviesAPI.Controllers
 {
-	[ApiController]
-	[Route("api/accounts")]
-	public class AccountsController : ControllerBase
-	{
-		private readonly UserManager<IdentityUser> userManager;
-		private readonly SignInManager<IdentityUser> signInManager;
+    [ApiController]
+    [Route("api/accounts")]
+    public class AccountsController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly SignInManager<IdentityUser> signInManager;
         private readonly IConfiguration configuration;
+        private readonly ApplicationDbContext context;
+        private readonly IMapper mapper;
 
         public AccountsController(UserManager<IdentityUser> userManager,
-			SignInManager<IdentityUser> signInManager, IConfiguration configuration)
-		{
-			this.userManager = userManager;
-			this.signInManager = signInManager;
+            SignInManager<IdentityUser> signInManager,
+            IConfiguration configuration,
+            ApplicationDbContext context,
+            IMapper mapper)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
             this.configuration = configuration;
+            this.context = context;
+            this.mapper = mapper;
         }
 
-		[HttpPost("create")]
-		public async Task<ActionResult<AuthenticationResponse>> Create(
-			[FromBody] UserCredentials userCredentials)
-		{
-			var user = new IdentityUser { UserName = userCredentials.Email, Email = userCredentials.Email };
-			var result = await userManager.CreateAsync(user, userCredentials.Password);
+        [HttpGet("listUsers")]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Policy = "IsAdmin")]
+        public async Task<ActionResult<List<UserDTO>>> GetListUsers([FromQuery] PaginationDTO paginationDTO)
+        {
+            var queryable = context.Users.AsQueryable();
+            await HttpContext.InsertParametersPaginationInHeader(queryable);
+            var users = await queryable.OrderBy(x => x.Email).Paginate(paginationDTO).ToListAsync();
+            return mapper.Map<List<UserDTO>>(users);
+        }
 
-			if (result.Succeeded)
-			{
-				return BuildToken(userCredentials);
-			}
-			else
-			{
-				return BadRequest(result.Errors);
-			}
-		}
+        [HttpPost("makeAdmin")]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Policy = "IsAdmin")]
+        public async Task<ActionResult> MakeAdmin([FromBody] string userId)
+        {
+            var user = await userManager.FindByIdAsync(userId);
+            await userManager.AddClaimAsync(user, new Claim("role", "admin"));
+            return NoContent();
+        }
 
-		[HttpPost("login")]
-		public async Task<ActionResult<AuthenticationResponse>> Login(
-			[FromBody] UserCredentials userCredentials)
-		{
-			var result = await signInManager.PasswordSignInAsync(userCredentials.Email,
-				userCredentials.Password, isPersistent: false, lockoutOnFailure: false);
+        [HttpPost("removeAdmin")]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Policy = "IsAdmin")]
+        public async Task<ActionResult> RemoveAdmin([FromBody] string userId)
+        {
+            var user = await userManager.FindByIdAsync(userId);
+            await userManager.RemoveClaimAsync(user, new Claim("role", "admin"));
+            return NoContent();
+        }
 
-			if (result.Succeeded)
-			{
-				return BuildToken(userCredentials);
-			}
-			else
-			{
-				return BadRequest("Incorrect Login");
-			}
-		}
+        [HttpPost("create")]
+        public async Task<ActionResult<AuthenticationResponse>> Create(
+            [FromBody] UserCredentials userCredentials)
+        {
+            var user = new IdentityUser { UserName = userCredentials.Email, Email = userCredentials.Email };
+            var result = await userManager.CreateAsync(user, userCredentials.Password);
 
-		private AuthenticationResponse BuildToken(UserCredentials userCredentials)
-		{
-			var claims = new List<Claim>()
-			{
-				new Claim("email", userCredentials.Email)
-			};
+            if (result.Succeeded)
+            {
+                return await BuildToken(userCredentials);
+            }
+            else
+            {
+                return BadRequest(result.Errors);
+            }
+        }
 
-			var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["keyjwt"]));
-			var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+        [HttpPost("login")]
+        public async Task<ActionResult<AuthenticationResponse>> Login(
+            [FromBody] UserCredentials userCredentials)
+        {
+            var result = await signInManager.PasswordSignInAsync(userCredentials.Email,
+                userCredentials.Password, isPersistent: false, lockoutOnFailure: false);
 
-			var expiration = DateTime.UtcNow.AddYears(1);
+            if (result.Succeeded)
+            {
+                return await BuildToken(userCredentials);
+            }
+            else
+            {
+                return BadRequest("Incorrect Login");
+            }
+        }
 
-			var token = new JwtSecurityToken(issuer: null, audience: null, claims: claims,
-				expires: expiration, signingCredentials: creds);
+        private async Task<AuthenticationResponse> BuildToken(UserCredentials userCredentials)
+        {
+            var claims = new List<Claim>()
+            {
+                new Claim("email", userCredentials.Email)
+            };
 
-			return new AuthenticationResponse()
-			{
-				Token = new JwtSecurityTokenHandler().WriteToken(token),
-				Expiration = expiration
-			};
-		}
-	}
+            var user = await userManager.FindByNameAsync(userCredentials.Email);
+            var claimsDB = await userManager.GetClaimsAsync(user);
+
+            claims.AddRange(claimsDB);
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["keyjwt"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var expiration = DateTime.UtcNow.AddYears(1);
+
+            var token = new JwtSecurityToken(issuer: null, audience: null, claims: claims,
+                expires: expiration, signingCredentials: creds);
+
+            return new AuthenticationResponse()
+            {
+                Token = new JwtSecurityTokenHandler().WriteToken(token),
+                Expiration = expiration
+            };
+        }
+    }
 }
-
